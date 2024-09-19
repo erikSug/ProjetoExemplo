0e1022155a91eefd2ac065b858fd15f5677fa6cb
Erik <eriksug4@gmail.com>
2024-09-19 11:23:09 -0300

    Criação de tipos de variáveis

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ console.log(nome3)
 let disciplina = null;
 console.log(disciplina)
 
+// Tipos de variáveis
+
+// Inteiro 
+let idade = 19;
+console.log(idade)
+
+// Flutuantes ou com casas decimais 
+let valor = 10.333333333333333333333;
+console.log(valor)
+
+// String (textos)
+let texto = "OMEGALUL";
+console.log(texto)
+
+// Booleanos - Verdadeiro ou falso
+let aluno = true 
+console.log(aluno)
+
+// Array - conjunto de informações 
+let tecnologias = ["JAVA","C#","HTML","CSS"];
+console.log(tecnologias)
+
